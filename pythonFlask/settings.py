6520bcafc45491ada86a0290915880c92681dde3
tk-tatsuro <tokuume.private@gmail.com>
2021-03-01 20:37:07 +0900

    ip add

--- pythonFlask/settings.py
+++ pythonFlask/settings.py
@@ -7,6 +7,7 @@ conf.read('settings.ini')
 access_token = conf['qiita']['access_token']
 
 port = conf['web']['port']
+ip = conf['web']['ip']
 
 db_name = conf['db']['name']
 db_driver = conf['db']['driver']
